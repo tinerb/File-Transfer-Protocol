2423877a7d768e00f5397cf7f3b36111cfbe83d5
tinerb <43251918+tinerb@users.noreply.github.com>
2019-02-28 17:42:38 -0500

    Add files via upload

--- Reciever.java
+++ Reciever.java
@@ -1,12 +1,13 @@
 package cp372_a2;
 
-// imports for the enter key action
-import java.awt.Desktop.Action;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 import javax.swing.AbstractAction;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
@@ -21,11 +22,14 @@ public class Reciever {
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private JTextField textField_3;
 	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
+	 * 
+	 * @throws SocketException
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SocketException {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -37,37 +41,39 @@ public class Reciever {
 				}
 			}
 		});
-		
-		
-		
+
 	}
 
 	/**
 	 * Create the application.
+	 * 
+	 * @throws UnknownHostException
+	 * @throws SocketException
+	 * @throws NumberFormatException
 	 */
-	public Reciever() {
+	public Reciever() throws UnknownHostException, NumberFormatException, SocketException {
 		initialize();
+		// InetAddress address = InetAddress.getByName(textField.getText());
+		// DatagramSocket socket = new
+		// DatagramSocket(Integer.parseInt(textField_1.getText()), address);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
+
 		// create the needed listeners
-		AbstractAction action = new AbstractAction()
-		{
-		    @Override
-		    public void actionPerformed(ActionEvent e)
-		    {
-		        System.out.println("some action");
-		    }
+		AbstractAction action = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("some action");
+			}
 		};
-		
-		
+
 		frame = new JFrame("Receiver");
-		
-		frame.setBounds(100, 100, 292, 244);
+
+		frame.setBounds(100, 100, 292, 357);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
@@ -80,7 +86,7 @@ public class Reciever {
 		textField.setBounds(176, 8, 86, 20);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Port# for Sender: ");
 		lblNewLabel_1.setBounds(10, 36, 142, 14);
 		frame.getContentPane().add(lblNewLabel_1);
@@ -95,7 +101,7 @@ public class Reciever {
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 
-		// Port# for Reciever text field 
+		// Port# for Reciever text field
 		textField_2 = new JTextField();
 		textField_2.setBounds(176, 58, 86, 20);
 		frame.getContentPane().add(textField_2);
@@ -122,15 +128,29 @@ public class Reciever {
 		textField_3.setColumns(10);
 
 		JLabel lblCurrentOfRecieved = new JLabel("Current# of Recieved Packets:");
-		lblCurrentOfRecieved.setBounds(10, 143, 208, 14);
+		lblCurrentOfRecieved.setBounds(10, 174, 208, 14);
 		frame.getContentPane().add(lblCurrentOfRecieved);
 
 		// Current# of Recieved Packets **DISPLAY ONLY** text field
 		textField_4 = new JTextField();
 		textField_4.setEditable(false);
-		textField_4.setBounds(10, 166, 165, 20);
+		textField_4.setBounds(10, 199, 165, 20);
 		frame.getContentPane().add(textField_4);
 		textField_4.setColumns(10);
-		
+
+		JButton btnNewButton = new JButton("Recieve");
+		btnNewButton.setBounds(10, 140, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+
+		JLabel lblNewLabel_3 = new JLabel("Output:");
+		lblNewLabel_3.setBounds(10, 230, 46, 14);
+		frame.getContentPane().add(lblNewLabel_3);
+
+		textField_5 = new JTextField();
+		textField_5.setEditable(false);
+		textField_5.setBounds(10, 255, 252, 52);
+		frame.getContentPane().add(textField_5);
+		textField_5.setColumns(10);
+
 	}
 }
