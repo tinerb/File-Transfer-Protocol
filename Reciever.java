9400e98068dcf3892681e8748590af510a6281c2
tinerb <43251918+tinerb@users.noreply.github.com>
2019-03-02 16:34:08 -0500

    Add files via upload

--- Reciever.java
+++ Reciever.java
@@ -2,8 +2,9 @@ package cp372_a2;
 
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 import javax.swing.AbstractAction;
 import javax.swing.ButtonGroup;
@@ -14,11 +15,13 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class Reciever {
+	public DatagramSocket ds = new DatagramSocket(3000);
+	public byte[] buf = new byte[1024];
+	public DatagramPacket dp = new DatagramPacket(buf, 1024);
 
 	private JFrame frame;
 	private JTextField textField;
 	private JTextField textField_1;
-	private JTextField textField_2;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private JTextField textField_3;
 	private JTextField textField_4;
@@ -27,9 +30,9 @@ public class Reciever {
 	/**
 	 * Launch the application.
 	 * 
-	 * @throws SocketException
+	 * @throws IOException
 	 */
-	public static void main(String[] args) throws SocketException {
+	public static void main(String[] args) throws IOException {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -41,28 +44,22 @@ public class Reciever {
 				}
 			}
 		});
-
 	}
 
 	/**
 	 * Create the application.
 	 * 
-	 * @throws UnknownHostException
-	 * @throws SocketException
 	 * @throws NumberFormatException
+	 * @throws IOException
 	 */
-	public Reciever() throws UnknownHostException, NumberFormatException, SocketException {
+	public Reciever() throws NumberFormatException, IOException {
 		initialize();
-		// InetAddress address = InetAddress.getByName(textField.getText());
-		// DatagramSocket socket = new
-		// DatagramSocket(Integer.parseInt(textField_1.getText()), address);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-
 		// create the needed listeners
 		AbstractAction action = new AbstractAction() {
 			@Override
@@ -91,64 +88,54 @@ public class Reciever {
 		lblNewLabel_1.setBounds(10, 36, 142, 14);
 		frame.getContentPane().add(lblNewLabel_1);
 
-		JLabel lblNewLabel_2 = new JLabel("Port# for Reciever: ");
-		lblNewLabel_2.setBounds(10, 61, 142, 14);
-		frame.getContentPane().add(lblNewLabel_2);
-
 		// Port# for Sender text field
 		textField_1 = new JTextField();
 		textField_1.setBounds(176, 33, 86, 20);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 
-		// Port# for Reciever text field
-		textField_2 = new JTextField();
-		textField_2.setBounds(176, 58, 86, 20);
-		frame.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
-
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Reliable");
 		buttonGroup.add(rdbtnNewRadioButton);
-		rdbtnNewRadioButton.setBounds(10, 110, 109, 23);
+		rdbtnNewRadioButton.setBounds(10, 82, 109, 23);
 		frame.getContentPane().add(rdbtnNewRadioButton);
 
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Unreliable");
 		buttonGroup.add(rdbtnNewRadioButton_1);
-		rdbtnNewRadioButton_1.setBounds(121, 110, 109, 23);
+		rdbtnNewRadioButton_1.setBounds(121, 82, 109, 23);
 		frame.getContentPane().add(rdbtnNewRadioButton_1);
 
 		JLabel lblNameOfFile = new JLabel("Name of File:");
-		lblNameOfFile.setBounds(10, 86, 142, 14);
+		lblNameOfFile.setBounds(10, 61, 142, 14);
 		frame.getContentPane().add(lblNameOfFile);
 
 		// Name of File text field
 		textField_3 = new JTextField();
-		textField_3.setBounds(176, 83, 86, 20);
+		textField_3.setBounds(176, 58, 86, 20);
 		frame.getContentPane().add(textField_3);
 		textField_3.setColumns(10);
 
 		JLabel lblCurrentOfRecieved = new JLabel("Current# of Recieved Packets:");
-		lblCurrentOfRecieved.setBounds(10, 174, 208, 14);
+		lblCurrentOfRecieved.setBounds(10, 146, 208, 14);
 		frame.getContentPane().add(lblCurrentOfRecieved);
 
 		// Current# of Recieved Packets **DISPLAY ONLY** text field
 		textField_4 = new JTextField();
 		textField_4.setEditable(false);
-		textField_4.setBounds(10, 199, 165, 20);
+		textField_4.setBounds(10, 171, 165, 20);
 		frame.getContentPane().add(textField_4);
 		textField_4.setColumns(10);
 
 		JButton btnNewButton = new JButton("Recieve");
-		btnNewButton.setBounds(10, 140, 89, 23);
+		btnNewButton.setBounds(10, 112, 89, 23);
 		frame.getContentPane().add(btnNewButton);
 
 		JLabel lblNewLabel_3 = new JLabel("Output:");
-		lblNewLabel_3.setBounds(10, 230, 46, 14);
+		lblNewLabel_3.setBounds(10, 202, 46, 14);
 		frame.getContentPane().add(lblNewLabel_3);
 
 		textField_5 = new JTextField();
 		textField_5.setEditable(false);
-		textField_5.setBounds(10, 255, 252, 52);
+		textField_5.setBounds(10, 227, 252, 80);
 		frame.getContentPane().add(textField_5);
 		textField_5.setColumns(10);
 
