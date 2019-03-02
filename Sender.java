afaa0cd906dd7e76da053063be66ac160cfa00a6
iLooL <Ruckas_x1@hotmail.com>
2019-03-02 17:17:34 -0500

    gui accepts input

--- Sender.java
+++ Sender.java
@@ -13,13 +13,19 @@ import javax.swing.JTextField;
 public class Sender {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JTextField ipAddressText;
+	private JTextField portNumText;
+	private JTextField filenameText;
+	private JTextField datagramSizeText;
+	private JTextField timeoutText;
+	private JTextField transmissionTime;
+	
+	public String ipAddress;
+	public int portNum;
+	public String filename;
+	public int maxDatagramSize;
+	public int timeout; // in microseconds
+	
 
 	/**
 	 * Launch the application.
@@ -50,17 +56,6 @@ public class Sender {
 	 */
 	private void initialize() {
 		
-		// create the needed listeners
-		AbstractAction action = new AbstractAction()
-		{
-		    @Override
-		    public void actionPerformed(ActionEvent e)
-		    {
-		        System.out.println("some action");
-		    }
-		};
-		
-		
 		frame = new JFrame("Sender");
 		frame.setBounds(100, 100, 295, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -71,92 +66,93 @@ public class Sender {
 		frame.getContentPane().add(lblNewLabel);
 
 		// Reciever IP Address text field
-		textField = new JTextField();
-		textField.setBounds(164, 8, 86, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		
-		textField.addActionListener(action);
+		ipAddressText = new JTextField();
+		ipAddressText.setBounds(164, 8, 86, 20);
+		frame.getContentPane().add(ipAddressText);
+		ipAddressText.setColumns(10);
 
 		JLabel lblNewLabel_1 = new JLabel("Port# for Reciever:");
 		lblNewLabel_1.setBounds(10, 36, 144, 14);
 		frame.getContentPane().add(lblNewLabel_1);
 
 		// Port# for Reciever text field
-		textField_1 = new JTextField();
-		textField_1.setBounds(164, 33, 86, 20);
-		frame.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-
-		JLabel lblPortForSender = new JLabel("Port# for Sender:");
-		lblPortForSender.setBounds(10, 61, 144, 14);
-		frame.getContentPane().add(lblPortForSender);
-
-		// Port# for Sender text field
-		textField_2 = new JTextField();
-		textField_2.setBounds(164, 58, 86, 20);
-		frame.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
+		portNumText = new JTextField();
+		portNumText.setBounds(164, 33, 86, 20);
+		frame.getContentPane().add(portNumText);
+		portNumText.setColumns(10);
 
 		JLabel lblNewLabel_2 = new JLabel("Name of File:");
 		lblNewLabel_2.setBounds(10, 86, 144, 14);
 		frame.getContentPane().add(lblNewLabel_2);
 
 		// Name of File text field
-		textField_3 = new JTextField();
-		textField_3.setBounds(164, 83, 86, 20);
-		frame.getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		filenameText = new JTextField();
+		filenameText.setBounds(164, 83, 86, 20);
+		frame.getContentPane().add(filenameText);
+		filenameText.setColumns(10);
 
 		JLabel lblNewLabel_3 = new JLabel("Max Size of Datagram:");
 		lblNewLabel_3.setBounds(10, 111, 144, 14);
 		frame.getContentPane().add(lblNewLabel_3);
 
 		// Max Size of Datagram text field
-		textField_4 = new JTextField();
-		textField_4.setBounds(164, 108, 86, 20);
-		frame.getContentPane().add(textField_4);
-		textField_4.setColumns(10);
+		datagramSizeText = new JTextField();
+		datagramSizeText.setBounds(164, 108, 86, 20);
+		frame.getContentPane().add(datagramSizeText);
+		datagramSizeText.setColumns(10);
 		
 		JLabel lblTimeout = new JLabel("Timeout(microseconds):  ");
 		lblTimeout.setBounds(10, 139, 157, 14);
 		frame.getContentPane().add(lblTimeout);
 
 		// Timeout(microseconds) **DISPLAY ONLY** text field
-		textField_5 = new JTextField();
-		textField_5.setBounds(164, 136, 86, 20);
-		frame.getContentPane().add(textField_5);
-		textField_5.setColumns(10);
+		timeoutText = new JTextField();
+		timeoutText.setBounds(164, 136, 86, 20);
+		frame.getContentPane().add(timeoutText);
+		timeoutText.setColumns(10);
 
-		JButton btnNewButton = new JButton("Transfer");
-		btnNewButton.setBounds(85, 175, 89, 23);
-		frame.getContentPane().add(btnNewButton);
+		JButton sendButton = new JButton("Transfer");
+		sendButton.setBounds(85, 175, 89, 23);
+		frame.getContentPane().add(sendButton);
 
 		JLabel lblNewLabel_4 = new JLabel("Transmission Time: ");
 		lblNewLabel_4.setBounds(10, 214, 144, 14);
 		frame.getContentPane().add(lblNewLabel_4);
 
 		// Transmission Time **DISPLAY ONLY** text field
-		textField_6 = new JTextField();
-		textField_6.setEditable(false);
-		textField_6.setBounds(164, 211, 86, 20);
-		frame.getContentPane().add(textField_6);
-		textField_6.setColumns(10);
+		transmissionTime = new JTextField();
+		transmissionTime.setEditable(false);
+		transmissionTime.setBounds(164, 211, 86, 20);
+		frame.getContentPane().add(transmissionTime);
+		transmissionTime.setColumns(10);
 		
-		btnNewButton.addActionListener(new ActionListener() {
+		sendButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(textField.getText().equals("") ||
-				   textField_1.getText().equals("") ||
-				   textField_2.getText().equals("") ||
-				   textField_3.getText().equals("") ||
-				   textField_4.getText().equals("") ||
-				   textField_5.getText().equals("") ||
-				   textField_6.getText().equals("")) {
+				if(ipAddressText.getText().equals("") ||
+				   portNumText.getText().equals("") ||
+				   filenameText.getText().equals("") ||
+				   datagramSizeText.getText().equals("") ||
+				   timeoutText.getText().equals("")) {
 					System.out.println("Please enter values for each text field to begin transfer");
 				}
-				
+				else {
+					portNum = Integer.parseInt(portNumText.getText());
+					ipAddress = ipAddressText.getText();
+					filename = filenameText.getText();
+					maxDatagramSize = Integer.parseInt(datagramSizeText.getText());	
+					timeout = Integer.parseInt(timeoutText.getText());
+					
+					
+					// input debugging
+					System.out.println("portNum: " + portNum + 
+							" ipAddress: " + ipAddress + 
+							" filename: " + filename +
+							" maxDatagramSize: " + maxDatagramSize +
+							" timeout: " + timeout + "\n");
+				}
+
+
 			}
 		});
 		
